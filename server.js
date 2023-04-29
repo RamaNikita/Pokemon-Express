c104b1d613c0160aeafc0347cbb35e4380d68d94
Nikita_Rama <nikita.rama10@gmail.com>
2023-04-29 15:44:57 -0500

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const pokemon = require("./models/pokemon");
 
+//Middleware
 const reactViewsEngine = require("jsx-view-engine").createEngine();
 app.engine("jsx", reactViewsEngine);
 app.set("view engine", "jsx");
 app.set("views", "./views");
+//
 app.get("/", (req, res) => {
   res.send("Welcome to the Pokemon App!");
 });
@@ -14,9 +16,15 @@ app.get("/", (req, res) => {
 // app.get("/pokemon", (req, res) => {
 //   res.send({ pokemon });
 // });
+//Index
 app.get("/pokemon", (req, res) => {
   res.render("Index", { pokemon });
 });
+//Show
+app.get("/pokemon/:id", (req, res) => {
+  res.send(req.params.id);
+});
+//Listen
 app.listen(PORT, () => {
   console.log(`Listening on port:${PORT}`);
 });
